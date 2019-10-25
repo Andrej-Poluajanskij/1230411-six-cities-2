8ada934da2ec12f68cd6f40e60fba8bc8269e1ad
Andrej-Poluajanskij <andrej_polujanskij@icloud.com>
2019-10-25 12:20:33 +0300

    Add reactDom package

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import WelcomeScreen from './components/welcome-screen/welcome-screen';
+
+const App = () => {
+  return (
+    <WelcomeScreen />
+  );
+};
+
+export default App;
