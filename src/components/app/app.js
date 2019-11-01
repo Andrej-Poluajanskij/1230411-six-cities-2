387f8934f2c214caaa053304c95ca7625b9a9dca
Andrej-Poluajanskij <andrej_polujanskij@icloud.com>
2019-11-01 17:07:43 +0200

    Update section about apartment

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import WelcomeScreen from '../welcome-screen/welcome-screen';
+const descriptions = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
+
+const App = () => {
+  return (
+    <WelcomeScreen aboutApartment={descriptions} />
+  );
+};
+
+export default App;
